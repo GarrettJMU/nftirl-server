e2552da8890f7657bf1afc15ac6c7b6c689559fe
Garrett Hughes <garretthughes3@gmail.com>
2021-08-29 16:25:06 -0400

    add in better regioning

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,22 +1,5 @@
 class LocationsController < ApplicationController
   def countries
-    render json: {countries: Country.all}.to_json, status: :ok
+    render json: {countries: City.pluck(:country).uniq}.to_json, status: :ok
   end
-end
-
-Rails.root.join('db', 'migrate', 'worldcities.csv')
-
-count = 0
-CSV.foreach(Rails.root.join('db', 'migrate', 'worldcities.csv')) do |row|
-  next if count > 0
-  puts row
-  count += 1
-end
-
-
-
-cities = CSV.read(Rails.root.join('db', 'migrate', 'worldcities.csv'), headers:true)
-
-cities.each do |row|
-  puts "#{row['Name']} - #{row['Phone']}"
 end

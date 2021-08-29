92f94ebb9b26d62992e0b1e688b8092b49a92227
Garrett Hughes <garretthughes3@gmail.com>
2021-08-29 16:28:58 -0400

    add in citiers routes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,14 @@ class LocationsController < ApplicationController
   def countries
     render json: {countries: City.pluck(:country).uniq}.to_json, status: :ok
   end
+
+  def countries_cities
+    results = []
+
+    if params[:country]
+      results = City.where(country: params[:country]).pluck(:city)
+    end
+
+    render json: {cities: results}, status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'internal#system_check'
   get '/countries', to: 'locations#countries'
+  get '/:country/cities', to: 'locations#countries_cities'
 end
